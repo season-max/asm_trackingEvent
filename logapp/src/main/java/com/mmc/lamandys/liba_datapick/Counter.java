e401fdc9aa94d26349b483dfe1f728ed93a80c75
yaoxinxin <yaoxinxin@zhangyue.com>
2022-06-13 23:42:36 +0800

    initial commit

---
+++ logapp/src/main/java/com/mmc/lamandys/liba_datapick/Counter.java
@@ -0,0 +1,15 @@
+package com.mmc.lamandys.liba_datapick;
+
+/**
+ * Created by xishuang on 2018/1/6.
+ */
+
+public class Counter {
+
+    public int test() throws InterruptedException {
+
+        int digit = 1;
+        return digit;
+
+    }
+}
